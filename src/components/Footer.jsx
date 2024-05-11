8c328b694013cdc56d4fac4a75ede967d75e536f
Marwa <alzokarimarwa@gmail.com>
2024-05-11 02:03:59 -0500

    footer end

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Container = styled.div`
 min-height: 100vh;
 background-color: var(--backColor);
 position: relative;
-border-top: 13px solid #ccc; /* Add border-top */
+border-bottom: 7px solid #ccc; /* Add border-top */
 top: 80px;
 
 h1 {
@@ -19,8 +19,18 @@ h1 {
     white-space: nowrap;
 }
 
+.contact {
+    position: absolute;
+    top: 4%;
+    left: 78%;
+    width: 30%;
+    padding: 20px;
+    color: var(--mainColor);
+    font-size: 25px;
+  }
+
 .footer{
-  transform: translate(2%, 4%);
+  transform: translate(2%, 7%);
 }
 
 /* Style the SVG path */
@@ -43,6 +53,9 @@ svg path {
   width: 50px; /* Adjust the width of the icons */
   height: auto; /* Maintain aspect ratio */
 }
+
+.contact{
+}
 `;
 
 function Footer() {
@@ -59,6 +72,9 @@ function Footer() {
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
         <path fill="#000000" d="M 11 4 C 7.1456661 4 4 7.1456661 4 11 L 4 39 C 4 42.854334 7.1456661 46 11 46 L 39 46 C 42.854334 46 46 42.854334 46 39 L 46 11 C 46 7.1456661 42.854334 4 39 4 L 11 4 z M 11 6 L 39 6 C 41.773666 6 44 8.2263339 44 11 L 44 39 C 44 41.773666 41.773666 44 39 44 L 11 44 C 8.2263339 44 6 41.773666 6 39 L 6 11 C 6 8.2263339 8.2263339 6 11 6 z M 13.085938 13 L 22.308594 26.103516 L 13 37 L 15.5 37 L 23.4375 27.707031 L 29.976562 37 L 37.914062 37 L 27.789062 22.613281 L 36 13 L 33.5 13 L 26.660156 21.009766 L 21.023438 13 L 13.085938 13 z M 16.914062 15 L 19.978516 15 L 34.085938 35 L 31.021484 35 L 16.914062 15 z"></path>
         </svg>
+        </div>
+        <div className='contact'>
+            <h3>contact us here: address</h3>
         </div>
         <h1> Ambiance Studio<span style={{ color: 'white' }}>.</span></h1>
     </Container>
